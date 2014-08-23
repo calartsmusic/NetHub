0d70f23603d6987b81526818392e1613408b58b3
scazan <scottcazan@gmail.com>
2014-08-23 10:10:55 -0700

    Adding a simple Linux install script

---
+++ install-linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#mkdir /Applications/SuperCollider/SuperCollider.app/Contents/Resources/SCClassLibrary/CalArts
+cp ./NetHub.sc /usr/share/SuperCollider/Extensions/NetHub.sc
+echo "NetHub copied into SC Class Library (if you see any errors above about a file existing, ignore them)"
+
